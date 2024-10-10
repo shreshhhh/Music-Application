d7da64461a58242f72bff2314c3c78d7df916cc7
Shrestha <120598649+shreshhhh@users.noreply.github.com>
2024-10-10 20:49:24 +0530

    Add song folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MusicForm from './MusicForm';
+import MusicList from './MusicList';
+import './index.css';
+import Footer from './Footer';
 
-function App() {
+const App = () => {
+  const [songs, setSongs] = useState([]);
+  const [selectedSong, setSelectedSong] = useState(null);
+
+  const fetchSongs = () => {
+    axios.get('http://localhost:3001/music')
+      .then(res => setSongs(res.data.music))
+      .catch(err => console.error(err));
+  };
+
+  const deleteSong = (id) => {
+    axios.delete(`http://localhost:3001/music/${id}`)
+      .then(() => fetchSongs()) 
+      .catch(err => console.error(err));
+  };
+
+  const editSong = (song) => {
+    setSelectedSong(song);
+  };
+
+  useEffect(() => {
+    fetchSongs();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Music Library</h1>
+      <MusicForm 
+        fetchSongs={fetchSongs} 
+        selectedSong={selectedSong} 
+        setSelectedSong={setSelectedSong} 
+      />
+      <MusicList 
+        songs={songs} 
+        deleteSong={deleteSong} 
+        editSong={editSong} 
+      />
+      <Footer/>
     </div>
   );
-}
+};
 
 export default App;
+
+
